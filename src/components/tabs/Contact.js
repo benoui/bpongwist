41640f9259801f18fe60771b5327939bed89a5eb
benoui <44357665+benoui@users.noreply.github.com>
2019-03-11 18:17:13 +0700

    add redux-form
    add reduxform and update contact component

--- src/components/tabs/Contact.js
+++ src/components/tabs/Contact.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Typography, TextField  } from '@material-ui/core';
 
-const Contact = () => {
-    return(
-        <div>Contact Me</div>
-    );
-};
+class Contact extends React.Component {
+    renderTextField({
+        input,
+        label,
+        ...custom
+    }){
+        return (
+            <TextField
+                label={label} 
+                {...input}
+                {...custom}
+                margin="normal"
+                variant="outlined"
+            />
+        );
+    }
 
-export default Contact; 
+    render(){  
+        return(
+            <div>
+                <Typography variant="h3" align="center" gutterBottom>
+                    CONTACT
+                </Typography>
+                <br/>
+                <form align="center">
+                    <div>
+                        <Field 
+                            name="name" 
+                            component={this.renderTextField}
+                            label="Name" />
+                    </div>
+                    <div>
+                        <Field 
+                            name="email" 
+                            component={this.renderTextField}
+                            label="Email" />
+                    </div>
+                    <div>
+                        <Field 
+                            name="message" 
+                            component={this.renderTextField}
+                            label="Message"
+                            multiline
+                            rows={5} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'contactForm'
+})(Contact);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import openWeatherReducer from './openWeatherReducer';
 
 export default combineReducers({
-    openWeather: openWeatherReducer
+    openWeather: openWeatherReducer,
+    form: formReducer
 });
