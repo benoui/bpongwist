384b324c5ecc85bc97ae4903aaa838593f0a0635
benoui <44357665+benoui@users.noreply.github.com>
2019-03-18 17:26:54 +0700

    Update Contact.js

--- src/components/tabs/Contact.js
+++ src/components/tabs/Contact.js
@@ -1,51 +1,65 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Typography, TextField  } from '@material-ui/core';
+import { Typography, TextField, Button } from '@material-ui/core';
+import { EmailOutlined } from '@material-ui/icons';
 
 class Contact extends React.Component {
-    renderTextField({
+    renderTextField = ({
         input,
+        meta: {touched, error},
         label,
         ...custom
-    }){
+    }) => {
         return (
-            <TextField
-                label={label} 
-                {...input}
-                {...custom}
-                margin="normal"
-                variant="outlined"
-            />
+            <div >
+                <TextField
+                    label={label}
+                    {...custom}
+                    {...input}
+                    margin="normal"
+                    variant="outlined"
+                    helperText={touched ? error : ''}
+                />
+            </div>
         );
     }
 
-    render(){  
-        return(
+    onSubmit(formValues) {
+    }
+
+    render() {
+        return (
             <div>
                 <Typography variant="h3" align="center" gutterBottom>
                     CONTACT
                 </Typography>
-                <br/>
-                <form align="center">
+                <br />
+                <form align="center" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                     <div>
-                        <Field 
-                            name="name" 
+                        <Field
+                            name="name"
                             component={this.renderTextField}
                             label="Name" />
                     </div>
                     <div>
-                        <Field 
-                            name="email" 
+                        <Field
+                            name="email"
                             component={this.renderTextField}
                             label="Email" />
                     </div>
                     <div>
-                        <Field 
-                            name="message" 
+                        <Field
+                            name="message"
                             component={this.renderTextField}
                             label="Message"
-                            multiline
-                            rows={5} />
+                            multiline={true}
+                            rows={4} />
+                    </div>
+                    <div>
+                        <Button type="submit" variant="outlined" color="primary"  >
+                            SUBMIT  
+                            <EmailOutlined />
+                        </Button>
                     </div>
                 </form>
             </div>
@@ -53,6 +67,21 @@ class Contact extends React.Component {
     }
 }
 
+const validate = formValues => {
+    const errors = {}
+    const requiredFields = [
+        'name',
+        'email'
+    ]
+    requiredFields.forEach(field => {
+        if (!formValues[field]) {
+            errors[field] = `Please fill out your ${field}`
+        }
+    })
+    return errors
+};
+
 export default reduxForm({
-    form: 'contactForm'
+    form: 'contactForm',
+    validate
 })(Contact);
